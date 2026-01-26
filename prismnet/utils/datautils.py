e113672a42ffda2c01fffef1d8c13f7f395da531
Guanyu Chen <C867469688@gmail.com>
2026-01-27 01:42:40 +0800

    Replace os.mkdir with os.makedirs to avoid mkdir conflicting on multithread training

--- prismnet/utils/datautils.py
+++ prismnet/utils/datautils.py
@@ -11,13 +11,13 @@ from copy import deepcopy
 def make_directory(path, foldername, verbose=1):
     """make a directory"""
 
-    if not os.path.isdir(path):
-        os.mkdir(path)
+    os.makedirs(path, exist_ok=True)
+    if verbose:
         print("making directory: " + path)
 
     outdir = os.path.join(path, foldername)
-    if not os.path.isdir(outdir):
-        os.mkdir(outdir)
+    os.makedirs(outdir, exist_ok=True)
+    if verbose:
         print("making directory: " + outdir)
     return outdir
 
